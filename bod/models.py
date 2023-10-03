aa5361dccece4cda831d13a0b1db7b213ac0ffd9
Piotr1103 <47223554+Piotr1103@users.noreply.github.com>
2023-10-03 10:22:21 +0800

    Update models.py

--- bod/models.py
+++ bod/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class word(models.Model):
+	id = models.AutoField(primary_key=True)
 	bod = models.TextField('藏文')
 	han = models.TextField('漢語')
 	category = models.PositiveSmallIntegerField('品詞', choices=[
